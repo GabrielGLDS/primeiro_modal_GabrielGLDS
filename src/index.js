514c3b2464d7cabb414183e2dccfd5bb891c8c17
GabrielGLDS <Gabeglds@hotmail.com>
2023-08-02 16:55:27 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 function handleModal(){
     const modalController = document.querySelector(".modal__controller")
     const button = document.querySelector(".header__button")
-
+    
     button.addEventListener("click", function(event){
         modalController.showModal()
     })
 }
+function closeModal(){
+    const modalController = document.querySelector(".modal__controller")
+    const closeButton = document.querySelector(".modal__close")
+    
+    closeButton.addEventListener("click", function(event){
+        modalController.close()
+    })
+    
+}
+
+function closeModalBackdrop(){
+    const modalController = document.querySelector(".modal__controller")
+    modalController.addEventListener("click", function(event){
+        // console.log(event.target)
+
+        if(event.target.classList.contains('modal__controller')=== true){
+            modalController.close()
+        }
+    })
+
+}
 
 handleModal()
+closeModal()
+closeModalBackdrop()
